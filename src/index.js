d6bdae77c94c0356f2a6fd322e26fe99b66b4feb
JongwonYoun <jeffyoun@naver.com>
2020-02-29 21:41:40 +0900

    init

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import rootReducer, { rootSaga } from "./modules";
+import { createLogger } from "redux-logger";
+import createSagaMiddleware from "redux-saga";
+import { composeWithDevTools } from "redux-devtools-extension";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const logger = createLogger();
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(logger, sagaMiddleware))
+);
+sagaMiddleware.run(rootSaga);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/test/simple.test.js
@@ -0,0 +1,23 @@
+function leapYear(n) {
+  if (n % 4 === 0) {
+    if (n % 100 === 0) {
+      if (n % 400 === 0) {
+        return true;
+      }
+      return false;
+    }
+    return true;
+  }
+  return false;
+}
+
+test("2020년은 윤년이다", () => {
+  expect(leapYear(2020)).toBe(true);
+});
+
+test("2019년은 윤년이 아니다", () => {
+  expect(leapYear(2019)).toBe(false);
+});
+test("1900년은 윤년이 아니다", () => {
+  expect(leapYear(1900)).toBe(false);
+});
