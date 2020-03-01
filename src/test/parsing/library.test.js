a9b6eaa7ec956095794e9fa5b9e369f4495c6468
hyunju <dlguswn3659@naver.com>
2020-03-01 22:53:33 +0900

    babel.config.js 수정, library.test 수정

--- src/test/parsing/library.test.js
+++ src/test/parsing/library.test.js
@@ -5,9 +5,43 @@ describe("library.js의 makePostRequest는", () => {
     const api = new libraryAPI();
     const data = await api.getLibrary();
     expect(data.seoul[0]).toHaveProperty('title');
-    expect(data.seoul[0]).toHaveProperty('remain');
-    expect(data.seoul[0]).toHaveProperty('use');
-    expect(data.seoul[0]).toHaveProperty('total');
+    expect(data.seoul[1]).toHaveProperty('remain');
+    expect(data.seoul[2]).toHaveProperty('use');
+    expect(data.seoul[3]).toHaveProperty('total');
     done();
   });
+
+  it("성공적으로 데이터를 불러오지 못하면 에러를 발생시킨다", () => {
+    const axiosMock = {};
+    axiosMock.post = () => {
+      return new Promise((resolve, reject) => {
+        reject();
+      });
+    };
+    const api = new libraryAPI(axiosMock);
+    expect(() => api.getLibrary().toThrow());
+  });
 });
+
+// describe("library.js의 getLibrary는", () => {
+//   it("데이터를 가져오는데 성공하면 올바른 객체 형태로 반환한다", async done => {
+//     const api = new libraryAPI();
+//     const data = await api.getLibrary();
+//     expect(data.seoul[0]).toHaveProperty('title');
+//     expect(data.seoul[0]).toHaveProperty('remain');
+//     expect(data.seoul[0]).toHaveProperty('use');
+//     expect(data.seoul[0]).toHaveProperty('total');
+//     done();
+//   });
+
+//   it("성공적으로 데이터를 불러오지 못하면 에러를 발생시킨다", () => {
+//     const axiosMock = {};
+//     axiosMock.post = () => {
+//       return new Promise((resolve, reject) => {
+//         reject();
+//       });
+//     };
+//     const api = new libraryAPI(axiosMock);
+//     expect(() => api.getLibrary().toThrow());
+//   });
+// });
