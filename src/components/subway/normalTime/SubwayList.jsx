435a05e91f72641172c4c570e4f1c00dc6c4f1c2
JongwonYoun <jeffyoun@naver.com>
2020-03-13 01:08:25 +0900

    지하철 로딩 추가

--- src/components/subway/normalTime/SubwayList.jsx
+++ src/components/subway/normalTime/SubwayList.jsx
@@ -4,6 +4,8 @@ import { getSubwayInfo } from "../../../lib/subway";
 
 function SubwayList({ line }) {
   let [lists, setLists] = useState([[], []]);
+  let [loading, setLoading] = useState(true);
+  let [error, setError] = useState(false);
 
   useEffect(() => {
     let updateId = null;
@@ -37,26 +39,29 @@ function SubwayList({ line }) {
     console.log("초 업데이트");
     console.log(count + 1);
     let results = [0, 1].map(index => {
-      return info[index].map(data => {
-        let date = new Date();
-        date.setSeconds(date.getSeconds() + data.time);
-        let secondRaw = data.time >= count ? data.time - count : 0;
+      return info[index]
+        .filter(data => data.last !== "0" && data.time !== 0)
+        .map(data => {
+          let date = new Date();
+          date.setSeconds(date.getSeconds() + data.time);
+          let secondRaw = data.time >= count ? data.time - count : 0;
 
-        let minute = parseInt(secondRaw / 60);
-        let second = secondRaw - minute * 60;
-        return (
-          <li
-            key={data.message + data.last + data.name}
-            className="subwayDetail"
-          >
-            <div className="subwayTime">
-              {date.getHours()} : {date.getMinutes()}
-            </div>
-            <div className="subwayLast">{data.last}</div>
-            <div className="subwayTimeMessage">{`${minute}분 ${second}초`}</div>
-          </li>
-        );
-      });
+          let minute = parseInt(secondRaw / 60);
+          let second = secondRaw - minute * 60;
+          setLoading(false);
+          return (
+            <li
+              key={data.message + data.last + data.name}
+              className="subwayDetail"
+            >
+              <div className="subwayTime">
+                {date.getHours()} : {date.getMinutes() > 10 ? date.getMinutes() : `0${date.getMinutes()}`}
+              </div>
+              <div className="subwayLast">{data.last}</div>
+              <div className="subwayTimeMessage">{`${minute}분 ${second}초`}</div>
+            </li>
+          );
+        });
     });
     // console.log(results);
     setLists(results);
@@ -64,17 +69,26 @@ function SubwayList({ line }) {
 
   return (
     <div className="subwayListWrapper">
-      <div className="subwayList">
-        <div className="subwayInfo">
-          <span>{line === 7 ? "장암행" : "중앙보훈병원행"}</span>
-          <ul>{lists[0]}</ul>
+      {loading && (
+        <div
+          class={`ui active centered inline loader ${
+            line === 7 ? "seven" : "nine"
+          }`}
+        ></div>
+      )}
+      {!loading && (
+        <div className="subwayList">
+          <div className="subwayInfo">
+            <span>{line === 7 ? "장암행" : "중앙보훈병원행"}</span>
+            <ul>{lists[0]}</ul>
+          </div>
+          <div className="vLine" />
+          <div className="subwayInfo">
+            <span>{line === 7 ? "부평구청행" : "개화행"}</span>
+            <ul>{lists[1]}</ul>
+          </div>
         </div>
-        <div className="vLine" />
-        <div className="subwayInfo">
-          <span>{line === 7 ? "부평구청행" : "개화행"}</span>
-          <ul>{lists[1]}</ul>
-        </div>
-      </div>
+      )}
     </div>
   );
 }
